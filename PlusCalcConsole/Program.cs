30d3d8223458436f57faa30ef56de9556c1cdb7e
Gebs <genbertdacudao@gmail.com>
2023-08-10 16:24:39 +0800

    Add project files.

---
+++ PlusCalcConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlusCalcConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Plus Calculator");
+
+            // Prompt for the first number
+            Console.Write("Enter the first number: ");
+            string input1 = Console.ReadLine();
+            double number1 = Convert.ToDouble(input1);
+
+            // Prompt for the second number
+            Console.Write("Enter the second number: ");
+            string input2 = Console.ReadLine();
+            double number2 = Convert.ToDouble(input2);
+
+            // Calculate the sum
+            double sum = number1 + number2;
+
+            // Display the result
+            Console.WriteLine($"The sum of {number1} and {number2} is: {sum}");
+        }
+    }
+}
---
+++ PlusCalcUnitTests/UnitTest1.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace PlusCalcUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+
+        {
+            // Redirect console output for testing
+            StringWriter sw = new StringWriter();
+            Console.SetOut(sw);
+
+            // Provide input values to simulate user input
+            string input = "3" + Environment.NewLine + "5" + Environment.NewLine;
+            StringReader sr = new StringReader(input);
+            Console.SetIn(sr);
+
+            // Run the program
+            PlusCalcConsole.Program.Main(null);
+
+            // Get the output
+            string output = sw.ToString();
+
+            // Assert the output contains the expected result
+            StringAssert.Contains(output, "The sum of 3 and 5 is: 8");
+
+            // Clean up
+            sw.Close();
+            sr.Close();
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+
+        {
+            // Redirect console output for testing
+            StringWriter sw = new StringWriter();
+            Console.SetOut(sw);
+
+            // Provide input values to simulate user input
+            string input = "3" + Environment.NewLine + "1" + Environment.NewLine;
+            StringReader sr = new StringReader(input);
+            Console.SetIn(sr);
+
+            // Run the program
+            PlusCalcConsole.Program.Main(null);
+
+            // Get the output
+            string output = sw.ToString();
+
+            // Assert the output contains the expected result
+            StringAssert.Contains(output, "The sum of 3 and 1 is: 5");
+
+            // Clean up
+            sw.Close();
+            sr.Close();
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+
+        {
+            // Redirect console output for testing
+            StringWriter sw = new StringWriter();
+            Console.SetOut(sw);
+
+            // Provide input values to simulate user input
+            string input = "2" + Environment.NewLine + "1" + Environment.NewLine;
+            StringReader sr = new StringReader(input);
+            Console.SetIn(sr);
+
+            // Run the program
+            PlusCalcConsole.Program.Main(null);
+
+            // Get the output
+            string output = sw.ToString();
+
+            // Assert the output contains the expected result
+            StringAssert.Contains(output, "The sum of 3 and 1 is: 5");
+
+            // Clean up
+            sw.Close();
+            sr.Close();
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+
+        {
+            // Redirect console output for testing
+            StringWriter sw = new StringWriter();
+            Console.SetOut(sw);
+
+            // Provide input values to simulate user input
+            string input = "10" + Environment.NewLine + "11" + Environment.NewLine;
+            StringReader sr = new StringReader(input);
+            Console.SetIn(sr);
+
+            // Run the program
+            PlusCalcConsole.Program.Main(null);
+
+            // Get the output
+            string output = sw.ToString();
+
+            // Assert the output contains the expected result
+            StringAssert.Contains(output, "The sum of 10 and 11 is: 21");
+
+            // Clean up
+            sw.Close();
+            sr.Close();
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+
+        {
+            // Redirect console output for testing
+            StringWriter sw = new StringWriter();
+            Console.SetOut(sw);
+
+            // Provide input values to simulate user input
+            string input = "2" + Environment.NewLine + "3" + Environment.NewLine;
+            StringReader sr = new StringReader(input);
+            Console.SetIn(sr);
+
+            // Run the program
+            PlusCalcConsole.Program.Main(null);
+
+            // Get the output
+            string output = sw.ToString();
+
+            // Assert the output contains the expected result
+            StringAssert.Contains(output, "The sum of 2 and 3 is: 5");
+
+            // Clean up
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
